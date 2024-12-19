b452edf864547307d146511fc792c5a7e40d4d4e
Akshad Gupta <akshad369@gmail.com>
2024-12-20 00:40:23 +0530

    days

--- portfolio/src/Header/Header.jsx
+++ portfolio/src/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const header = () => {
   return (
     <header style={{marginTop:"0px",zIndex:"2"}}>
-      <h1 style={{marginTop:"0"}}>Hi everyBody</h1>
+      <h1 style={{marginTop:"0",padding:"15px",backgroundColor:"#152b61",boxShadow: "5px 5px 15px rgba(0, 0, 0, 0.9)",fontSize:"30px"}}> <span style={{marginLeft:"75px"}}>Akshad Gupta</span> </h1>
     </header>
   )
 }
